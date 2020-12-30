11bcb79306046e3e4703b4e6a6e206df3a861dd7
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-30 12:13:29 +0530

    akita: The full Google Pixel experience!
    * Set the correct features to make apps such as
      Google Camera work.
    
    Thanks fuzzywave40!
    
    Change-Id: I9f04238928599d9e319584b178986c34c6a5b097

--- device-akita.mk
+++ device-akita.mk
@@ -33,6 +33,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# Google Camera
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/sysconfig/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
+
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworkResOverlayProductAkita \
