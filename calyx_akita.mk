f610e1aaa9e92916d81a6db05b0ca4cedd25b2de
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-04 01:01:42 +0530

    akita: Set up proprietary files extraction
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    
    Co-authored-by: Aaron Kling <webgeek1234@gmail.com>
    Co-authored-by: Cosmin Tanislav <demonsingur@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I17b78ccf212d8364070c40fe3c7d06d4048e3fe4

--- calyx_akita.mk
+++ calyx_akita.mk
@@ -11,6 +11,7 @@ $(call inherit-product, vendor/calyx/config/common_phone.mk)
 # Inherit device configuration
 DEVICE_CODENAME := akita
 DEVICE_PATH := device/google/akita
+VENDOR_PATH := vendor/google/akita
 $(call inherit-product, device/google/zuma/calyx_common.mk)
 $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-calyx.mk)
 $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
@@ -29,3 +30,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := google/akita/akita:14/AP2A.240905.003.A1/12234140:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
