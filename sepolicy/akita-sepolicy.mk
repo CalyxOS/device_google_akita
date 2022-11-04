04a10e8f28e6a2e0e341a1608f50917731c2e644
Michael Bestas <mkbestas@lineageos.org>
2022-11-04 03:28:51 +0200

    akita: Update sepolicy path
    Change-Id: Ia24ef37000ed9e1cdf994a32447b0250e3f89186

--- sepolicy/akita-sepolicy.mk
+++ sepolicy/akita-sepolicy.mk
@@ -1,8 +1,8 @@
 # sepolicy exclusively for akita.
-BOARD_SEPOLICY_DIRS += device/google/akita-sepolicy/aam
-BOARD_SEPOLICY_DIRS += device/google/akita-sepolicy/vendor
-BOARD_SEPOLICY_DIRS += device/google/akita-sepolicy/tracking_denials
-BOARD_SEPOLICY_DIRS += device/google/akita-sepolicy/radio
+BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/aam
+BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/vendor
+BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/tracking_denials
+BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/radio
 
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/gps/pixel/sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
