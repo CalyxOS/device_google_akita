a0b4626c23faf49d6030f53f89b7b318b0c85bcb
Michael Bestas <mkbestas@lineageos.org>
2022-12-22 01:02:18 +0200

    shusky: overlay: Configure aux cameras for Aperture
    Change-Id: I3d1c93057b5c2a1ba09c06a945d538d2043f30fd

--- akita/device-calyx.mk
+++ akita/device-calyx.mk
@@ -1,3 +1,5 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/akita/akita/overlay-calyx
+
 TARGET_VENDOR_PROP += device/google/akita/akita/vendor.prop
 
 $(call inherit-product, device/google/akita/device-calyx.mk)
