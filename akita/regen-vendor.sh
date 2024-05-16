38a0975c60b9521656d517eb119323717afded0f
Chirayu Desai <chirayudesai1@gmail.com>
2024-05-16 23:21:31 +0530

    shiba camera and wifi ext apex
    Change-Id: I6abd236de77b55a15ff82796ae1344d2b48cc817

--- akita/regen-vendor.sh
+++ akita/regen-vendor.sh
@@ -911,6 +911,11 @@ VENDOR_SKIP_FILES=(
 
     # Google
     "etc/default-permissions/default-permissions_talkback.xml"
+
+    # Shiba
+    "apex/com.google.pixel.camera.hal.apex"
+    "apex/com.google.pixel.wifi.ext.apex"
+    "lib64/vendor.google.wifi_ext-V1-ndk.so"
 )
 
 # Initialize the helper
