c5374a376c995de6d8083199752bc72ebd18de20
Michael Bestas <mkbestas@gmail.com>
2025-06-24 08:59:13 +0300

    akita: Remove HIDL audio configuration
    Change-Id: I45e4f8521c352fe05b247040be9001b2e0e1ae93

--- device-akita.mk
+++ device-akita.mk
@@ -25,9 +25,7 @@ endif
 
 DEVICE_PACKAGE_OVERLAYS += device/google/akita/akita/overlay
 
-ifeq ($(RELEASE_PIXEL_AIDL_AUDIO_HAL_ZUMA),true)
 USE_AUDIO_HAL_AIDL := true
-endif
 
 include device/google/akita/audio/akita/audio-tables.mk
 include device/google/zuma/device-shipping-common.mk
@@ -229,24 +227,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.bqr.vnd_quality_mask=16 \
     persist.bluetooth.bqr.vnd_trace_mask=0 \
     persist.bluetooth.vendor.btsnoop=false
-
-ifeq ($(USE_AUDIO_HAL_AIDL),true)
-# AIDL
-
-else
-# HIDL
-
-# Spatial Audio
-PRODUCT_PACKAGES += \
-	libspatialaudio \
-	librondo
-
-# Sound Dose
-PRODUCT_PACKAGES += \
-	android.hardware.audio.sounddose-vendor-impl \
-	audio_sounddose_aoc \
-
-endif
 
 # HdMic Audio
 PRODUCT_PROPERTY_OVERRIDES += \
